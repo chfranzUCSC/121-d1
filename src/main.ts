000aebdf26b596e4a92f96cafea4561f10b40e73
chfranzUCSC <chfranz@ucsc.edu>
2025-10-17 22:34:20 +0000

    Step 1 complete. Added a clickable button.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+const button1 = document.createElement("button") as HTMLButtonElement;
+// as- typescript thing. HTMLButtonElement to stop it from wriggling into a different shape?
+button1.textContent = "🦎🦎🦎";
+// textContent adds plain text- innerHTML can have formatting i.e. <br>
+document.body.appendChild(button1);
+// append to document body to actually add the thing to the page object (document object model)
