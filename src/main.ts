6aeccf65f931aa6299d5e316aa2b507252700aff
chfranzUCSC <chfranz@ucsc.edu>
2025-10-17 23:01:14 +0000

    Step 2 complete. Added a counter that increments when the button is pressed.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import "./style.css";
 
-const button1 = document.createElement("button") as HTMLButtonElement;
+let counterNumber: number = 0;
+
+const counterDiv = document.createElement("div") as HTMLDivElement;
+counterDiv.textContent = `${counterNumber} Lizards`;
+document.body.append(counterDiv);
+
+const clickerButton = document.createElement("button") as HTMLButtonElement;
 // as- typescript thing. HTMLButtonElement to stop it from wriggling into a different shape?
-button1.textContent = "🦎🦎🦎";
+clickerButton.textContent = "🦎🦎🦎";
 // textContent adds plain text- innerHTML can have formatting i.e. <br>
-document.body.appendChild(button1);
+document.body.appendChild(clickerButton);
 // append to document body to actually add the thing to the page object (document object model)
+
+
+clickerButton.addEventListener("click", () => {
+  counterNumber++;
+  counterDiv.textContent = `${counterNumber} Lizards`;
+});
